5094f03a76cf0592c2d6e28336a3d274bb6707fe
nikit <nikitaaa6@yandex.ru>
2022-10-19 01:11:39 +0800

    homework6

---
+++ src/state6.java
@@ -0,0 +1,12 @@
+public class state6 {public static void main(String[] args) {System.out.println("задание про боесеров или задание 6");
+    var boxerweight1=78.2;
+    System.out.println("Вес одного боксера " + boxerweight1+" кг.");
+    var boxerweight2=82.7;
+    System.out.println("Вес второго боксера " + boxerweight2+" кг.");
+    var totalweight=boxerweight1+boxerweight2;
+    System.out.println("общий вес двух бойцов "+totalweight+" кг.");
+    var differenceweight=boxerweight1-boxerweight2;
+        differenceweight=-differenceweight;
+    System.out.println("разница между весами бойцов "+differenceweight+" кг.");
+}
+}
